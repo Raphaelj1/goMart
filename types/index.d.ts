0e6a11fbc82c46eb18dc467816f2a7328070e97c
Raphaelj <raphaeljames897@gmail.com>
2026-05-26 16:36:04 +0100

    chore: add global schema type declarations

---
+++ types/index.d.ts
@@ -0,0 +1,132 @@
+type OrderStatus = 'ORDER_PLACED' | 'PROCESSING' | 'SHIPPED' | 'DELIVERED';
+type PaymentMethod = 'COD' | 'STRIPE';
+
+interface User {
+	id: string;
+	name: string;
+	email: string;
+	image: string;
+	cart: Record<string, number>;
+
+	// Relations (Optional)
+	ratings?: Rating[];
+	Address?: Address[];
+	store?: Store | null;
+	buyerOrders?: Order[];
+}
+
+interface Product {
+	id: string;
+	name: string;
+	description: string;
+	mrp: number;
+	price: number;
+	images: string[];
+	category: string;
+	inStock: boolean;
+	storeId: string;
+	createdAt: string | Date;
+	updatedAt: string | Date;
+
+	// Relations (Optional)
+	store?: Store;
+	// orderItems?: OrderItem[];
+	rating?: Rating[];
+}
+
+interface Order {
+	id: string;
+	total: number;
+	status: OrderStatus;
+	userId: string;
+	storeId: string;
+	addressId: string;
+	isPaid: boolean;
+	paymentMethod: PaymentMethod;
+	isCouponUsed: boolean;
+	coupon?: Coupon;
+	createdAt: string | Date;
+	updatedAt: string | Date;
+
+	// Relations (Optional)
+	user?: User;
+	store?: Store;
+	address?: Address;
+	orderItems: OrderItem[];
+}
+
+interface OrderItem {
+	orderId: string;
+	productId: string;
+	quantity: number;
+	price: number;
+
+	// Relations (Optional)
+	order?: Order;
+	product: Product;
+}
+
+interface Rating {
+	id: string;
+	rating: number;
+	review: string;
+	userId: string;
+	productId: string;
+	orderId: string;
+	createdAt: string | Date;
+	updatedAt: string | Date;
+
+	// Relations (Optional)
+	user?: User;
+	product?: Product;
+}
+
+interface Address {
+	id: string;
+	userId: string;
+	name: string;
+	email: string;
+	street: string;
+	city: string;
+	state: string;
+	zip: string;
+	country: string;
+	phone: string;
+	createdAt: string | Date;
+
+	// Relations (Optional)
+	Order?: Order[];
+	user?: User;
+}
+
+interface Coupon {
+	code: string;
+	description: string;
+	discount: number;
+	forNewUser: boolean;
+	forMember: boolean;
+	isPublic: boolean;
+	expiresAt: string | Date;
+	createdAt: string | Date;
+}
+
+interface Store {
+	id: string;
+	userId: string;
+	name: string;
+	description: string;
+	username: string;
+	address: string;
+	status: string;
+	isActive: boolean;
+	logo: string;
+	email: string;
+	contact: string;
+	createdAt: string | Date;
+	updatedAt: string | Date;
+
+	// Relations (Optional)
+	Product?: Product[];
+	Order?: Order[];
+	user?: User;
+}
