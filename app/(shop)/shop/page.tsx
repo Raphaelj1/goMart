a91c1f8340529090ffb95da5125ba5125e6c8858
Raphaelj <raphaeljames897@gmail.com>
2026-05-27 12:46:04 +0100

    feat: proper loading state in shop page

--- app/(shop)/shop/page.tsx
+++ app/(shop)/shop/page.tsx
@@ -1,24 +1,36 @@
 import { Suspense } from 'react';
 import ShopGrid from '@/components/shop/ShopGrid';
-import Loading from '@/components/Loading';
+import ShopSkeleton from '@/components/shop/ShopSkeleton';
 
 interface ShopPageProps {
 	searchParams: Promise<{ search?: string }>;
 }
 
-const ShopPage = async ({ searchParams }: ShopPageProps) => {
+interface ShopContentProps {
+	currentSearch: string;
+}
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function ShopContent({ currentSearch }: ShopContentProps) {
+	// fetch products
+
+	await delay(500); // simulate server delay
+
+	return <ShopGrid initialSearch={currentSearch} />;
+}
+
+export default async function ShopPage({ searchParams }: ShopPageProps) {
 	const resolvedParams = await searchParams;
 	const currentSearch = resolvedParams.search || '';
 
 	return (
 		<div className="min-h-[70vh] mx-6">
 			<div className="max-w-7xl mx-auto">
-				<Suspense fallback={<Loading />}>
-					<ShopGrid initialSearch={currentSearch} />
+				<Suspense key={currentSearch} fallback={<ShopSkeleton />}>
+					<ShopContent currentSearch={currentSearch} />
 				</Suspense>
 			</div>
 		</div>
 	);
-};
-
-export default ShopPage;
+}
