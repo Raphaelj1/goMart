8f5bb1828d33aa7bc67a8b883a1663117146d7c2
Raphaelj <raphaeljames897@gmail.com>
2026-06-09 19:42:55 +0100

    feat: inngest public API route

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,13 @@
 import { serve } from 'inngest/next';
 import { inngest } from '@/inngest/client';
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from '@/inngest/functions';
+import {
+	deleteCouponOnExpiry,
+	syncUserCreation,
+	syncUserDeletion,
+	syncUserUpdation,
+} from '@/inngest/functions';
 
 export const { GET, POST, PUT } = serve({
 	client: inngest,
-	functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
+	functions: [syncUserCreation, syncUserUpdation, syncUserDeletion, deleteCouponOnExpiry],
 });
